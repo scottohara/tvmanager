53eb3b181ba75f1561fe6a4c1e5b854e2a52f6f9
Scott O'Hara <scott@oharagroup.net>
2020-05-28 12:12:51 +1000

    Upgrade to karma@5.0.9, mocha@7.2.0, sinon@9.0.2 & sinon-chai@3.5.0

--- karma.conf.js
+++ karma.conf.js
@@ -45,6 +45,7 @@ module.exports = config => {
 		 * Continuous Integration mode
 		 * if true, Karma captures browsers, runs the tests and exits
 		 */
-		singleRun: true
+		singleRun: true,
+		failOnSkippedTests: true
 	}));
 };
